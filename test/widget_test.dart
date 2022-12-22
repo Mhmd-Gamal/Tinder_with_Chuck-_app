9b660c329b00b54e282f80a52807f2daf4ab8b91
Mhmd-Gamal <gemomy2015@gmail.com>
2022-12-22 19:10:08 +0300

    Commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,7 @@ import 'package:tinder_with_chuck_norris/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(ChuckNorris());
+    await tester.pumpWidget(const ChuckNorris());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
